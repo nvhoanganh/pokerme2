bc3fc6b3400cc7f4ae22e47fc376c0c2acccf8e3
nvhoanganh <nvhoanganh1909@gmail.com>
2020-05-03 12:56:20 +1000

    load list of sessions

--- src/Home.svelte
+++ src/Home.svelte
@@ -4,15 +4,40 @@
   import "firebase/firestore";
   import { navigateTo } from "svelte-router-spa";
 
+  let sessions = [];
   let user;
+
   firebase.auth().onAuthStateChanged(function(u) {
     user = u;
     if (user) {
-      console.log(user);
-    } else {
-      console.log("user is not logged in");
+      getListofSessions();
     }
   });
+
+  function viewSession(id) {
+    navigateTo("/sessions/" + id);
+  }
+
+  function getListofSessions() {
+    firebase
+      .firestore()
+      .collection("sessions")
+      .where("ownerId", "==", user.uid)
+      .get()
+      .then(function(querySnapshot) {
+        let list = [];
+        querySnapshot.forEach(function(doc) {
+          list.push({
+            id: doc.id,
+            ...doc.data()
+          });
+        });
+        sessions = list;
+      })
+      .catch(function(error) {
+        console.log("Error getting documents: ", error);
+      });
+  }
 
   function login() {
     firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider());
@@ -26,6 +51,7 @@
     const db = firebase.firestore();
     db.collection("sessions")
       .add({
+        ownerId: user.uid,
         ownerName: user.displayName,
         createdDate: new Date()
       })
@@ -56,8 +82,17 @@
     Login
   </button>
 {/if}
-<div class="text-3xl py-8">Previous Stories</div>
+<div class="text-3xl py-8">Previous Sessions</div>
 <ol>
-  <li>story 1</li>
-  <li>story 2</li>
+  {#each sessions as item}
+    <li>
+      {item.createdDate.toDate().toString()}
+      <button
+        on:click={() => viewSession(item.id)}
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4
+        rounded">
+        View
+      </button>
+    </li>
+  {/each}
 </ol>
---
+++ src/stores.js
@@ -0,0 +1,5 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import { navigateTo } from 'svelte-router-spa';
+
